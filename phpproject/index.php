4dd88585ffbd870c83078f212cf4352f062af63d
Dave Haslam <me@example.com>
2024-02-11 16:59:42 +0000

    first php code

---
+++ phpproject/index.php
@@ -0,0 +1,12 @@
+<?php
+// index.php
+
+echo "<h1>PHP Test Page</h1>";
+
+echo "The current date and time is: " . date('Y-m-d H:i:s');
+
+echo "<br>";
+
+echo "Server: " . $_SERVER['SERVER_NAME'];
+
+?>
